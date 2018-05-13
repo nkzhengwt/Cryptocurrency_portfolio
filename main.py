5ea9f54e46c88afd7c426ad45fcf2bd9c6d5b08b
Wentao Zheng <x1019575585@163.com>
2018-05-13 23:57:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,22 @@ Created on Fri May 11 14:38:47 2018
 from get_quo import *
 from cacu import *
 import pandas as pd
-from dateutil.parser import parse
 if __name__ == '__main__':
     # Set startdate
-    startdate = 20170101
+    startdate = 20171130
     # Set enddate
     enddate = 20171231
     # Set coin pool that you want to backtest
     # Examples:
     # coins = ['bitcoin','ethereum','ripple'] cryptocurrency you want to backtest or
-    # coins = CoinName()[:n]  top n cryptocurrency of virtual currency market
-    coins=CoinNames()[:3]
-    comb = list()
-    for i in range(len(coins)):
-        coin_name=coins[i]
-        Gather(startdate,enddate,coin_name)
-        temp= pd.read_csv(coin_name+'.csv')
-        temp1=temp['Close']
-        temp1.index=temp['Date'].apply(parse)
-        temp1.name=coin_name
-        comb.append(temp1)
-    comb2=pd.DataFrame(comb)
-    data=comb2.T
-    data=data.sort_index()
-    cacu(data,startdate,enddate)
+    # coins = CoinName(n)  top n cryptocurrency of virtual currency market
+    # coins = CoinName() all cryptocurrency of virtual currency market
+    coins=CoinNames(10)
+    # parsing data from coinmarketcap
+    get_data(startdate,enddate,coins)
+    # constructing portfolio and backtesting
+    Port = Portfolio(startdate,enddate,coins)
+    Port.combine_data()
+    Port.caculate()
+    Port.backtest()
+
