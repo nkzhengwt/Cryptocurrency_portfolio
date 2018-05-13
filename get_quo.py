cbb5b5bed7a2d2e9443d8541574a8db38094e415
Wentao Zheng <x1019575585@163.com>
2018-05-14 00:00:03 +0800

    Update get_quo.py

--- get_quo.py
+++ get_quo.py
@@ -11,22 +11,24 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-def CoinNames():
+def CoinNames(n=None):
     """Gets ID's of all coins on cmc"""
 
     names = []
     response = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=0")
     respJSON = json.loads(response.text)
-    for i in respJSON:
+    for i in respJSON[:n]:
         names.append(i['id'])
     return names
 
-def gather(startdate, enddate, names):
-    historicaldata = []
+def get_data(startdate, enddate, names):
     counter = 1
-
     for coin in names:
-        r  = requests.get("https://coinmarketcap.com/currencies/{0}/historical-data/?start={1}&end={2}".format(coin, startdate, enddate))
+        print(coin+' begins.')
+        historicaldata = []
+        r  = requests.get( \
+       "https://coinmarketcap.com/currencies/{0}/historical-data/?start={1}&end={2}" \
+       .format(coin, startdate, enddate))
         data = r.text
         soup = BeautifulSoup(data, "html.parser")
         table = soup.find('table', attrs={ "class" : "table"})
@@ -35,48 +37,28 @@ def gather(startdate, enddate, names):
         if len(historicaldata) == 0:
             headers = [header.text for header in table.find_all('th')]
             headers.insert(0, "Coin")
-
+        temp = 0
         for row in table.find_all('tr'):
             currentrow = [val.text for val in row.find_all('td')]
             if(len(currentrow) != 0):
                 currentrow.insert(0, coin)
             historicaldata.append(currentrow)
-
-        print("Coin Counter -> " + str(counter), end='\n')
+            temp = temp + 1
+            print('parsing '+ coin +' ' + str(temp)+'-th sucessfully!')
+        FILE_NAME = coin + ".csv"
+        with open(FILE_NAME, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            writer.writerows(row for row in historicaldata if row)
+        print("Coin Counter -> " + str(counter))
+        print('Coin Name -> ' + coin)
         counter += 1
-    return headers, historicaldata
-
-def Gather(startdate, enddate,coin_names):
-    """ Scrape data off cmc"""
-
-    if(coin_names == 'all'):
-        names = CoinNames()
-    else:
-        names = [coin_names]
-
-    headers, historicaldata = gather(startdate, enddate, names)
-
-    Save(headers, historicaldata,coin_names)
-
-def Save(headers, rows,coin_names):
-
-    if(coin_names== 'all'):
-        FILE_NAME = "HistoricalCoinData.csv"
-    else:
-        FILE_NAME = coin_names + ".csv"
-
-    with open(FILE_NAME, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(headers)
-        writer.writerows(row for row in rows if row)
     print("Finished!")
+
 
 if __name__ == "__main__":
 
     startdate = 20170101
-    enddate = 20170301
-    coin_names='all'
-#    headers, historicaldata = gather(startdate, enddate,coin_names)
-    Gather(startdate,enddate,coin_names)
-#    Save(headers,historicaldata,coin_names)
-
+    enddate = 20171231
+    coin_names=CoinNames(10)
+    getdata(startdate,enddate,coin_names)
